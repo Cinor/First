464ca5edc323b93df7c8cd1a93f1732ecd92d0b0
EN8859 <EN8859@SKEN8859>
2017-12-04 09:47:17 +0800

    登入驗證(Windows 內建)

---
+++ First(登入驗證)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="First_登入驗證_.MvcApplication" Language="C#" %>
---
+++ First(登入驗證)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(First_登入驗證_.Startup))]
+namespace First_登入驗證_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
